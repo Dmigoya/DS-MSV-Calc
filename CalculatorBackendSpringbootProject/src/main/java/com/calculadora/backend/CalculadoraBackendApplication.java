d081d16fbc8980a564b5e921fbaf2f0093ff83e6
Limon300 <droidelucio@gmail.com>
2025-12-06 16:43:51 +0100

    Creacion del proyecto spring

---
+++ CalculatorBackendSpringbootProject/src/main/java/com/calculadora/backend/CalculadoraBackendApplication.java
@@ -0,0 +1,20 @@
+package com.calculadora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CalculadoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraBackendApplication.class, args);
+	}
+        
+        @Bean
+        public RestTemplate RestTemplate() {
+            return new RestTemplate();
+        }
+
+}
---
+++ CalculatorBackendSpringbootProject/src/test/java/com/calculadora/backend/CalculadoraBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.calculadora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraBackendApplicationTests {
+        /*
+	@Test
+	void contextLoads() {
+            assert(true);
+	}
+        */
+
+}
